60d34bdcbeff4871c9a6f9bb113d5b5fe2973351
hellwind2019 <daniltorianik2016@gmail.com>
2024-03-26 14:54:36 +0200

    made other changes

--- modals/modal.ts
+++ modals/modal.ts
@@ -2,6 +2,7 @@ import test from "node:test";
 import { App, FuzzySuggestModal, Modal, Notice, Setting } from "obsidian";
 import { stringify } from "querystring";
 //TODO: Modal that receives one word, shows it asks for translation and return pair of words.
+//TODO: Add testing to do to the main branch
 export class NewWordModal extends Modal {
   result: string;
   word : string;
